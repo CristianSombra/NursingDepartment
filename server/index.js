83b992623fb1bb8bd059f8c8a726f5b7af5afe66
CristianSombra <cristiansombra87@gmail.com>
2023-10-22 20:17:55 -0300

    Depuración de código

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
 const app = require("./src/app");
 const {sequelize} = require("./src/db");
-// const {loadSectorInDb} = require("./src/data/sectorDb");
+
 
 //inicio servidor
 app.listen(3001, () => {
     sequelize.sync({force: false}); //sincronizo la base de datos
-
-
-// setTimeout(() => {
-//     loadSectorInDb();
-// },3000)
-
-console.log("Listening on port 3001");
+    console.log("Listening on port 3001");
 
 });
 
