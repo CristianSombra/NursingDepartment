ce23e2a126be471eabfc9c95a9fdbc6144011730
Cristian Sombra <cristiansombra87@gmail.com>
2023-11-16 11:48:15 -0300

    Landing Page
    Estilo y funcionalidad Landing Page

--- client/src/Views/Home/Home.jsx
+++ client/src/Views/Home/Home.jsx
@@ -1,17 +1,27 @@
 import CardsAgentsContainer from "../../Components/cardsContainer/cardsAgentsContainer/cardsAgentsContainer";
 import CardsSectorsContainer from "../../Components/cardsContainer/cardsSectorsContainer/cardsSectorsContainer";
+import img from '../../assets/404.jpg';
 
 const  Home = () => {
     return (
-        <div class="container">
-            <h1>Ésta es la view de Home</h1>
-            <h2>Aquí están los agentes</h2>
-            <CardsAgentsContainer/>
-            <h2>Aquí están los sectores</h2>
-            <CardsSectorsContainer/>
+        <div className="container d-flex align-items-center justify-content-center vh-100">
+            <div className="row">
+                <div className="col text-center">
+                    <img src={img} className="img-fluid" alt="404" />
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+    }
+    
+    
+    export default Home;
 
 
-export default Home;
+
+
+    {/* <h1>Ésta es la view de Home</h1>
+    <h2>Aquí están los agentes</h2>
+    <CardsAgentsContainer/>
+    <h2>Aquí están los sectores</h2>
+    <CardsSectorsContainer/> */}
--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import fondo from '../../assets/background.jpg'
 import './Landing.css'
 
 const Landing = () => {
     return (
-        <div class="container-fluid background">
+        <div class="container-fluid">
         <div class="container-intro d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
             <div class="row">
-                <div class="col text-center">
-                    <h2 class="title">Departamento de Enfermería</h2>
-                    <h3 class="subtitle">Recurso humano</h3>
-                    <button class="btn btn-dark">Ingresar</button>
+                <img src={fondo} class="img-fluid" alt="fondo" />
+                    <div class="col text-center">
+                    <h1 class="title text-black">Departamento de Enfermería</h1>
+                    <h2 class="subtitle text-muted">Base de datos del personal</h2>
+                    <Link to="/home">
+                        <button class="my-5 btn btn-outline-dark btn-lg">Ingresar</button>
+                    </Link>
                 </div>
             </div>
         </div>
