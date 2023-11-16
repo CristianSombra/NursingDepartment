3adc0fdd168315aee4ef5353815b43e1d0350f55
Cristian Sombra <cristiansombra87@gmail.com>
2023-11-16 10:13:56 -0300

    Se incorpora landing page
    Se incorpora Landing Page, falta funcionalidad.

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,9 +1,20 @@
-
+import React from 'react';
+import './Landing.css'
 
 const Landing = () => {
-    return(
-        <h1>Ésta es la view de Landing</h1>
-    )
+    return (
+        <div class="container-fluid background">
+        <div class="container-intro d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+            <div class="row">
+                <div class="col text-center">
+                    <h2 class="title">Departamento de Enfermería</h2>
+                    <h3 class="subtitle">Recurso humano</h3>
+                    <button class="btn btn-dark">Ingresar</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default Landing;
