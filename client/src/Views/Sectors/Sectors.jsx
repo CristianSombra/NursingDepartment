cdbdfe10a5ee001575f26f76efd55b3e2e87c97f
Cristian Sombra <cristiansombra87@gmail.com>
2023-11-20 15:48:07 -0300

    Navbar, Home.
    Se realiza actualización de Navbar y Home.

--- client/src/Views/Sectors/Sectors.jsx
+++ client/src/Views/Sectors/Sectors.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import CardsAgentsContainer from "../../Components/cardsContainer/cardsAgentsContainer/cardsAgentsContainer";
 import ScrollTop from "../../Components/scroll-to-top/scrollTop";
+import CardsSectorsContainer from "../../Components/cardsContainer/cardsSectorsContainer/cardsSectorsContainer";
 
-
-const  Home = () => {
+const  Sectors = () => {
     
     return (
         <div className="container align-items-center">
             <div className="row">
                 <div className="col">
                     <div className="my-5 text-center">
-                        <h2>Todos los agentes</h2>
+                        <h2>Todos los sectores</h2>
                     </div>
-                        <CardsAgentsContainer/>
+                        <h2>Aquí están los sectores</h2>
+                        <CardsSectorsContainer/>
+                    </div>
                 </div>
-            </div>
             <ScrollTop/>
         </div>
     );
 }
 
 
-export default Home;
+export default Sectors;
