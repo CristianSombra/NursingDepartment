b10297a0aa1451c8c0278812ec6128e543726e98
Cristian Sombra <cristiansombra87@gmail.com>
2023-11-28 22:45:22 -0300

    Sector Form
    Sector Form: Se crea lógica en el formulario para crear sector. Respuesta 200 ok. Sector se carga correctamente.

--- client/src/Views/Form/newSector.jsx
+++ client/src/Views/Form/newSector.jsx
@@ -1,20 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { CreateSectorHandler } from "../../Components/handlers/handlers";
 
 
 const NewSector = () => {
     const [imageUrl, setImageUrl] = useState("");
+    const [sector, setSector] = useState({
+        "id_sector": "",
+        "image": "",
+        "name": "",
+        "state": ""
+    })
 
+
+    const createSector = CreateSectorHandler();
+    
     const handleImageChange = (e) => {
         const selectedImage = e.target.files[0];
         setImageUrl(URL.createObjectURL(selectedImage));
     };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setSector((prevSector) => ({
+            ...prevSector,
+            [name]: value
+        }));
+    };
+
+    const handleAreaChange = (areaType) => {
+        setSector((prevSector) => ({
+            ...prevSector,
+            state: areaType
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await createSector(sector);
+        } catch (error) {
+            console.error('Error al crear el sector', error)
+        }
+    }
 
 
     return (
         <div className="container">
             <div className="row mt-5 d-flex justify-content-center">
                 <div className="col-12 col-md-10">
-                    <form action="">
+                    <form onSubmit={handleSubmit}>
                         <div className="row mt-5">
                             <div className="col-12 col-md-6">
                                 <div className="mb-3">
@@ -38,26 +72,63 @@ const NewSector = () => {
                             <div className="col-12 col-md-6">
                                 <div className="mb-3">
                                     <label for="nombre del servicio" className="form-label">Nombre del servicio</label>
-                                    <input type="text" className="form-control" placeholder="Ejemplo: Unidad de terapia intensiva pediátrica"/> 
+                                    <input 
+                                        type="text" 
+                                        className="form-control"
+                                        name="name"
+                                        value={sector.name}
+                                        onChange={handleChange} 
+                                        placeholder="Ejemplo: Unidad de terapia intensiva pediátrica"/> 
                                 </div>
                                 <div className="mb-3">
                                     <label for="id del servicio" className="form-label">ID del servicio</label>
-                                    <input type="text" className="form-control" placeholder="Ejemplo: UTIP"/>
+                                    <input 
+                                        type="text" 
+                                        className="form-control"
+                                        name="id_sector"
+                                        value={sector.id_sector}
+                                        onChange={handleChange} 
+                                        placeholder="Ejemplo: UTIP"/>
                                 </div>
                         
                                 <div className="mb-3">
-                                    <label for="tipo de área" className="form-label">Tipo de área</label>
-                                    <div className="form-check">
-                                        <input type="radio" className="form-check-input" name="tipo de área" id="área abierta"/>
-                                        <label for="área abierta" className="form-check-label">Área abierta</label>
-                                    </div>
-                                    <div className="form-check">
-                                        <input type="radio" className="form-check-input" name="tipo de área" id="área cerrada"/>
-                                        <label for="área cerrada" className="form-check-label">Área cerrada</label>
-                                    </div>
-                                </div>
+    <label htmlFor="tipo de área" className="form-label">
+        Tipo de área
+    </label>
+    <div className="form-check">
+        <input
+            type="radio"
+            className="form-check-input"
+            name="tipo de área"
+            id="área abierta"
+            onChange={() => handleAreaChange('Área abierta')}
+            checked={sector.state === 'Área abierta'}
+        />
+        <label htmlFor="área abierta" className="form-check-label">
+            Área abierta
+        </label>
+    </div>
+    <div className="form-check">
+        <input
+            type="radio"
+            className="form-check-input"
+            name="tipo de área"
+            id="área cerrada"
+            onChange={() => handleAreaChange('Área cerrada')}
+            checked={sector.state === 'Área cerrada'}
+        />
+        <label htmlFor="área cerrada" className="form-check-label">
+            Área cerrada
+        </label>
+    </div>
+</div>
                             </div>  
                         </div>
+                            <div className="mt-3 text-center">
+                                <button type="submit" className="btn btn-outline-dark">
+                                    Crear Sector
+                                </button>
+                            </div>
                     </form>
                 </div>
             </div>
