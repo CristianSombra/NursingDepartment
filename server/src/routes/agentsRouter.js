6c395226643e1f849d8af17d14d0402a2618b765
Cristian Sombra <cristiansombra87@gmail.com>
2024-01-09 00:28:19 -0300

    getAgentByName
    Se inicia endpoint para buscar agente por nombre.

--- server/src/routes/agentsRouter.js
+++ server/src/routes/agentsRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { getAllAgents, getAgentById , createAgent } = require('../handlers/agentsHandler');
+const { getAllAgents, getAgentById, getAgentByName, createAgent } = require('../handlers/agentsHandler');
 const agentsRouter = Router();
 
 
 agentsRouter.get('/', getAllAgents);
 agentsRouter.get('/:id', getAgentById);
+agentsRouter.get('/agent', getAgentByName);
 agentsRouter.post('/newagent', createAgent);
 
 module.exports = agentsRouter;
